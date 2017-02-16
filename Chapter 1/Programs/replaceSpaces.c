f38fd512265be86230f81e664a92d96ea9e0aa0c
= <=>
2017-02-15 16:11:54 -0800

    Did the C version of 1.4

---
+++ Chapter 1/Programs/replaceSpaces.c
@@ -0,0 +1,53 @@
+/* 1.4
+ * Write a method to replace all spaces in a string with '%20'. You
+ * may assume that the string has sufficient space at the end of the
+ * string to hold the additional characters, and that you are given
+ * the "true" length of the string. (Notes: If implementing in Java,
+ * please use a character array so that you can perform this operation
+ * in place.)
+*/
+#include <stdio.h>
+
+char* replace(char* str, int length) {
+	int spaces = 0;
+	int ptr = 0;
+
+	// Find the number of spaces in the string
+	for (int j = 0; j < length; j++) {
+		if (str[j] == ' ') {
+			spaces++;
+		}
+	}
+	/* 
+	   Find the final length of how long the string will be after
+	   the spaces have been replaced.
+	*/
+	int finalLength = length + (spaces * 2);
+	// Null-terminate the string
+	str[finalLength] = '\0';
+	// Start from the back and replace each space with '%20'
+	for (int k = (length - 1); k >= 0; k--) {
+		if (str[k] == ' ') {
+			str[finalLength - ptr - 1] = '0';
+			str[finalLength - ptr - 2] = '2';
+			str[finalLength - ptr - 3] = '%';
+			ptr = ptr + 3;
+		}
+		else {
+			str[finalLength - ptr - 1] = str[k];
+			ptr++;
+		}
+	}
+	return str;
+}
+
+int main(int argc, char** argv) {
+	char* str = argv[1];
+	int length = 0;
+
+	for (int i = 0; str[i] != '\0'; i++) {
+		length++;
+	}
+	char* newStr = replace(str, length);
+	printf("The new string is %s\n", newStr);
+}
