e1e36dbed607582a64002f64036702fe20bc3156
= <=>
2017-02-15 16:14:35 -0800

    Filled in file 4.c inside of the Questions folder

--- Chapter 1/Questions/4.c
+++ Chapter 1/Questions/4.c
@@ -7,3 +7,35 @@
  * in place.)
 */
 
+char* replace(char* str, int length) {
+	int spaces = 0;
+	int ptr = 0;
+
+	// Find the number of spaces in the string
+	for (int j = 0; j < length; j++) {
+		if (str[j] == ' ') {
+			spaces++;
+		}
+	}
+	/* 
+	   Find the final length of how long the string will be after
+	   the spaces have been replaced.
+	*/
+	int finalLength = length + (spaces * 2);
+	// Null-terminate the string
+	str[finalLength] = '\0';
+	// Start from the back and replace each space with '%20'
+	for (int k = (length - 1); k >= 0; k--) {
+		if (str[k] == ' ') {
+			str[finalLength - ptr - 1] = '0';
+			str[finalLength - ptr - 2] = '2';
+			str[finalLength - ptr - 3] = '%';
+			ptr = ptr + 3;
+		}
+		else {
+			str[finalLength - ptr - 1] = str[k];
+			ptr++;
+		}
+	}
+	return str;
+}
